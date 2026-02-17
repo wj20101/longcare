d20eb5c68de3dac1d617372d54ef4d3ea9e71835
yuyingui <yu.yingui@yalla.com>
2026-02-17 19:39:35 +0800

    refactor: move servicecountdown viewmodel into feature module

--- feature/servicecountdown/build.gradle.kts
+++ feature/servicecountdown/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("longcare.android.library")
     id("longcare.kotlin.common")
+    alias(libs.plugins.dagger.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -8,7 +10,14 @@ android {
 }
 
 dependencies {
+    implementation(project(":core:common"))
+    implementation(project(":core:domain"))
     implementation(project(":core:model"))
+    implementation(libs.androidx.core.ktx)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation(libs.bundles.hilt)
+    ksp(libs.dagger.hilt.compiler)
+    ksp(libs.hilt.compiler)
     testImplementation(libs.junit)
 }
