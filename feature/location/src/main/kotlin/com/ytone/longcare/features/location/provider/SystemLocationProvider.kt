1ba9be0f02ca69cd2d08c3d10a5aa4d85c01e214
yuyingui <yu.yingui@yalla.com>
2026-02-17 14:04:49 +0800

    refactor(location): replace deprecated cancellation signal usage

--- feature/location/src/main/kotlin/com/ytone/longcare/features/location/provider/SystemLocationProvider.kt
+++ feature/location/src/main/kotlin/com/ytone/longcare/features/location/provider/SystemLocationProvider.kt
@@ -3,9 +3,8 @@ package com.ytone.longcare.features.location.provider
 import android.annotation.SuppressLint
 import android.location.Location
 import android.location.LocationManager
-import androidx.core.content.ContextCompat
+import android.os.CancellationSignal
 import androidx.core.location.LocationManagerCompat
-import androidx.core.os.CancellationSignal
 import com.ytone.longcare.common.utils.logE
 import com.ytone.longcare.common.utils.logI
 import com.ytone.longcare.domain.location.LocationProvider
